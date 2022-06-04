4155b6ab56be7ae74e92c8c87d755183f0013a2c
ankitafate <102223526+ankitafate@users.noreply.github.com>
2022-06-04 17:16:27 +0530

    Update sonar.groovy

--- sonar.groovy
+++ sonar.groovy
@@ -1,10 +1,33 @@
 pipeline {
-  agent any 
-  stages {
-    stage("pull") {
-      steps {
-        git 'https://github.com/shubhamkalsait/studentapp-ui.git'
-      }
+    agent any 
+    stages {
+        stage('pull') { 
+            steps {
+               git 'https://github.com/shubhamkalsait/studentapp-ui.git' 
+            }
+        }
+        stage('build') { 
+            steps {
+               sh 'mvn clean package'
+            }
+        }
+       stage('test') { 
+            steps { withSonarQubeEnv ('sonar') {
+                sh 'mvn sonar:sonar'
+            }
+        }
     }
-  }
+        stage('QualityCheck') { 
+            steps { 
+                timeout(time: 1, unit: 'HOURS') {
+                    waitForQualityGate true
+                }  
+            }
+        }
+        stage('deploy') { 
+            steps {
+               deploy adapters: [tomcat8(credentialsId: 'ad', path: '', url: 'http://13.38.137.6:8080')], contextPath: '/', war: ' **/*.war'
+            }
+        }
+    }
 }
