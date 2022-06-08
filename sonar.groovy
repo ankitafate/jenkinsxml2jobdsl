86cf158056ce0ebfe7027fb0b5b53a40b893118f
ankitafate <102223526+ankitafate@users.noreply.github.com>
2022-06-08 14:51:48 +0530

    Update sonar.groovy

--- sonar.groovy
+++ sonar.groovy
@@ -12,9 +12,10 @@ pipeline {
             }
         }
        stage('test') { 
-            steps  withSonarQubeEnv ('sonar') {
+           steps { withSonarQubeEnv ('sonar') {
                 sh 'mvn sonar:sonar'
             }
+          }
         }
         stage('QualityCheck') { 
             steps { 
